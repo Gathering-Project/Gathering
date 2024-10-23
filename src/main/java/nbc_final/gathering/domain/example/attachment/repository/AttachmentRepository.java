5cac4e04f0e3ed95f6a54101cc69afa54f4fa048
wanduek <minjoois2@gmail.com>
2024-10-23 10:59:19 +0900

    fix: 첨부파일 수정 및 삭제 403에러 해결

--- src/main/java/nbc_final/gathering/domain/example/attachment/repository/AttachmentRepository.java
+++ src/main/java/nbc_final/gathering/domain/example/attachment/repository/AttachmentRepository.java
@@ -1,12 +1,14 @@
 package nbc_final.gathering.domain.example.attachment.repository;
 
-import nbc_final.gathering.common.dto.AuthUser;
 import nbc_final.gathering.domain.example.attachment.entity.Attachment;
 import nbc_final.gathering.domain.gathering.entity.Gathering;
+import nbc_final.gathering.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 
 public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
-    Attachment findByUser(AuthUser user);
-    Attachment findByUserAndGathering(AuthUser authUser, Gathering gathering);
+   List<Attachment> findByUser(User user);
+   List<Attachment> findByUserAndGathering(User user, Gathering gathering);
 }
