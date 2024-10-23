a38c59ef97fbb42d95d1ab1887cc012e80f42a85
fargoe <fargoewave@gmail.com>
2024-10-23 14:40:25 +0900

    fix : 예외 메세지 수정, 이벤트 Response data 수정

--- src/main/java/nbc_final/gathering/domain/event/dto/response/EventResponseDto.java
+++ src/main/java/nbc_final/gathering/domain/event/dto/response/EventResponseDto.java
@@ -4,29 +4,35 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import nbc_final.gathering.domain.event.entity.Event;
 
+import java.time.LocalDateTime;
+
 @Getter
 @AllArgsConstructor
 public class EventResponseDto {
 
     private Long eventId;
+    private Long userId;
     private String title;
     private String description;
     private String date;
     private String location;
     private Integer maxParticipants;
     private Integer currentParticipants;
-    private Long userId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public static EventResponseDto of(Event event, Long userId) {
         return new EventResponseDto(
                 event.getId(),
+                userId,
                 event.getTitle(),
                 event.getDescription(),
                 event.getDate(),
                 event.getLocation(),
                 event.getMaxParticipants(),
                 event.getCurrentParticipants(),
-                userId
+                event.getUpdatedAt(),
+                event.getCreatedAt()
         );
     }
 }
