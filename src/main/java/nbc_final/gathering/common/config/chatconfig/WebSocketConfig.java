014d4ede32b7353e28d6a96bd72152f62165deb6
wanduek <minjoois2@gmail.com>
2024-11-15 02:33:53 +0900

    fix: endpoint rename

--- src/main/java/nbc_final/gathering/common/config/chatconfig/WebSocketConfig.java
+++ src/main/java/nbc_final/gathering/common/config/chatconfig/WebSocketConfig.java
@@ -33,7 +33,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // socketJs 클라이언트가 WebSocket 핸드셰이크를 하기 위해 연결할 endpoint를 지정할 수 있다.
-        registry.addEndpoint("/chat/inbox")
+        registry.addEndpoint("/gathering/inbox")
                 .setAllowedOriginPatterns("*") // cors 허용을 위해 꼭 설정해주어야 함. setCredential() 설정시에 AllowedOrigin 과 같이 사용될 경우 오류가 날 수 있으므로 OriginPatterns 설정으로 사용하였음
                 .addInterceptors(new JwtHandshakeInterceptor(jwtUtil));
     }
