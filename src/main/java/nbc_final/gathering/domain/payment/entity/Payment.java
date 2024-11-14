df9c4b62822bca8651c1d69913dd20975965fdee
fargoe <fargoewave@gmail.com>
2024-11-15 00:46:27 +0900

    fix : payments js 오류 수정

--- src/main/java/nbc_final/gathering/domain/payment/entity/Payment.java
+++ src/main/java/nbc_final/gathering/domain/payment/entity/Payment.java
@@ -9,8 +9,6 @@ import nbc_final.gathering.domain.ad.entity.Ad;
 import nbc_final.gathering.domain.gathering.entity.Gathering;
 import nbc_final.gathering.domain.user.entity.User;
 
-import java.time.LocalDateTime;
-
 @Getter
 @Entity
 @Table(name = "payments")
@@ -46,22 +44,35 @@ public class Payment extends TimeStamped {
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ad_id", referencedColumnName = "adId")
-    private Ad ad;  // Ad 엔티티와의 연관관계 설정
+    private Ad ad;  // Ad와의 연관관계 추가
 
-    // Ad 객체를 파라미터로 받도록 수정
-    public static Payment create(User user, Long amount, String orderName, Ad ad, Gathering gathering) {
+    // setAd 메서드 추가
+    public void setAd(Ad ad) {
+        this.ad = ad;
+    }
+
+    // startProcessing 메서드 추가
+    public boolean startProcessing() {
+        if (isProcessing) {
+            return false; // 이미 처리 중이면 false 반환
+        }
+        isProcessing = true;
+        return true; // 처리 시작
+    }
+
+    // setOrderId 메서드 추가
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public static Payment create(User user, Long amount, String orderName, Gathering gathering) {
         Payment payment = new Payment();
         payment.user = user;
         payment.amount = amount;
         payment.orderName = orderName;
-        payment.ad = ad;
         payment.gathering = gathering;
-        payment.status = PayStatus.READY;
+        payment.status = PayStatus.READY; // READY 상태로 초기화
         return payment;
-    }
-
-    public void setOrderId(String orderId) {
-        this.orderId = orderId;
     }
 
     public void failPayment(String failReason) {
@@ -78,27 +89,6 @@ public class Payment extends TimeStamped {
     public void cancelPayment(String cancelReason) {
         this.status = PayStatus.CANCELED;
         this.cancelReason = cancelReason;
-    }
-
-    // 추가된 getter 메서드들
-    public Long getPaymentId() {
-        return this.paymentId;
-    }
-
-    public Ad getAd() {
-        return this.ad;
-    }
-
-    public synchronized boolean startProcessing() {
-        if (isProcessing) {
-            return false;
-        }
-        isProcessing = true;
-        return true;
-    }
-
-    public synchronized void endProcessing() {
-        isProcessing = false;
     }
 
     public boolean isPaySuccess() {
